812e7877fb6f7bd4623ecc67a4d8cbe471ff32ab
EmmanuelRuiz5307 <emmaprofemx@gmail.com>
2022-07-08 01:45:22 -0500

    Creacion de la clase Inicio y aplicando metodo

--- PDV-MODOCONSOLA/ComprarItem.cs
+++ PDV-MODOCONSOLA/ComprarItem.cs
@@ -7,6 +7,9 @@ namespace PDV_MODOCONSOLA
     class ComprarItem
     {//Inicio de la clase Item
 
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public int Cantidad { get; set; }
 
     }//Fin de la clase Item
 }
--- PDV-MODOCONSOLA/Inicio.cs
+++ PDV-MODOCONSOLA/Inicio.cs
@@ -4,12 +4,37 @@ using System.Text;
 
 namespace PDV_MODOCONSOLA
 {
+    public enum AdminOperation
+    {
+        AgregarItem = 1 ,  
+        ActualizarItem = 2, 
+        DesplegarItem = 3 , 
+        Cerrar = 4
+    }
+
     class Inicio
     {//Empieza la clase Inicio
 
 
+        public int EntradaUsuario(string inputPrompt , string errorPrompt)
+        {
+            //Mostrando mensaje de entrada
+            Console.WriteLine(inputPrompt);
+            var input = Console.ReadLine();
 
+            try{
+                return Convert.ToInt32(input);
+            }catch (Exception)
+            {
+                Console.WriteLine(errorPrompt);
+                return EntradaUsuario(inputPrompt, errorPrompt);
+            }
+
+        }
 
 
     }//Fin de la clase Inicio
+
+
+
 }
